e6780ef6743127615eed3f9aeb9215a903e13343
Francisco Liu <pancholiu@hotmail.com>
2025-12-19 21:35:01 -0400

    Create simple database as dict and improve formatting

--- app/main.py
+++ app/main.py
@@ -4,18 +4,62 @@ from scalar_fastapi import get_scalar_api_reference
 
 app = FastAPI()
 
-@app.get("/shipment/{id}")
-def get_shipment(id: int) -> dict[str,  Any]:
-    return {
-        "id": id,
+shipments = {
+    12701: {
         "weight": 1.2,
         "content": "wooden table",
         "status": "in transit"
+    },
+    12702: {
+        "weight": 0.8,
+        "content": "glassware set",
+        "status": "fragile - in transit"
+    },
+    12703: {
+        "weight": 2.5,
+        "content": "box of books",
+        "status": "delivered"
+    },
+    12704: {
+        "weight": 4.3,
+        "content": "electronics - laptop",
+        "status": "out for delivery"
+    },
+    12705: {
+        "weight": 0.4,
+        "content": "pair of shoes",
+        "status": "pending"
+    },
+    12706: {
+        "weight": 6.0,
+        "content": "flat-pack furniture",
+        "status": "delayed"
+    },
+    12707: {
+        "weight": 1.1,
+        "content": "children's toys",
+        "status": "cancelled"
     }
+}
+
+
+@app.get("/shipment/latest")
+def get_latest_shipment() -> dict[str, Any]:
+    latest_id = max(shipments.keys())
+    return shipments[latest_id]
+
+
+@app.get("/shipment/{id}")
+def get_shipment(id: int) -> dict[str,  Any]:
+    if id not in shipments:
+        return {"error": "Shipment not found"}
+
+    return shipments[id]
+
 
 @app.get("/scalar", include_in_schema=False)
 def get_scalar():
     return get_scalar_api_reference(
-        openapi_url = app.openapi_url,
+        openapi_url=app.openapi_url,
         title="Scalar API"
     )
