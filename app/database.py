fc50f6ffb296a7a00ab53a727dc716d0549a594d
Francisco Liu <pancholiu@hotmail.com>
2025-12-24 11:52:59 -0400

    database fix

--- app/database.py
+++ app/database.py
@@ -4,21 +4,31 @@ connections = sqlite3.connect("sqlite.db")
 cursor = connections.cursor()
 
 # 1. Create a table
-cursor.execute("""
+create_table = """
     CREATE TABLE IF NOT EXISTS shipment (
         id INTEGER, 
         content TEXT, 
         weight REAL, 
         status TEXT)
-""")
-
+"""
 
 # 2. Add shipment data
-cursor.execute("""
+insert_values = """
     INSERT INTO SHIPMENT
-               VALUES ('12702', 'basalt', 18.7, 'in_transit')
-""")
-connections.commit()
+               VALUES ('12703', 'metal', 15, 'placed')
+"""
+
+# 3. Read shipment by id
+read_shipment_by_id = """
+    SELECT * FROM shipment WHERE id = 12702
+"""
+
+cursor.execute(read_shipment_by_id)
+
+results = cursor.fetchone()
+
+print(results)
+
 
 # Close the connection
 connections.close()
