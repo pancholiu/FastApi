5e70c76a03d3dea253a53e27269aef82a602ee51
Francisco Liu <francisco.gao@mccain.ca>
2025-12-26 14:44:05 -0400

    Create SQLModel

---
+++ app/database/models.py
@@ -0,0 +1,21 @@
+# sqlmodel is an extension of Pydantic with some extra configurations
+from sqlmodel import SQLModel, Field, DateTime
+from enum import Enum
+
+
+class ShipmentStatus(str, Enum):
+    placed = "placed"
+    in_transit = "in_transit"
+    out_for_delivery = "out_for_delivery"
+    delivered = "delivered"
+
+
+class Shipment(SQLModel):
+    __table__ = "shipment"
+
+    id: int = Field(primary_key=True)
+    content: str
+    weight: float = Field(le=25)
+    destination: int
+    status: ShipmentStatus
+    estimated_delivery: DateTime
--- app/schemas.py
+++ app/schemas.py
@@ -1,22 +1,15 @@
 from random import randint
 from pydantic import BaseModel, Field
-from enum import Enum
+from app.database.models import ShipmentStatus
 
 
 def random_destination():
     return randint(11000, 12000)
 
 
-class ShipmentStatus(str, Enum):
-    placed = "placed"
-    in_transit = "in_transit"
-    out_for_delivery = "out_for_delivery"
-    delivered = "delivered"
-
-
 class BaseShipment(BaseModel):
     content: str | None = None
-    weight: float = Field(le=25, default=None)
+    weight: (float | None) = Field(le=25, default=None)
     destination: int | None = None
 
 
