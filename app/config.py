948a784b345cab55da1cdc45ce5dba871eec9c74
Francisco Liu <pancholiu@hotmail.com>
2025-12-28 18:43:49 -0400

    Add config file

---
+++ app/config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class DatabaseSetting(BaseSettings):
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: int
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+
+    model_config = SettingsConfigDict(
+        env_file="./.env",
+        env_ignore_empty=True,
+        extra="ignore"
+    )
+
+
+settings = DatabaseSetting()
+
+print(settings.POSTGRES_PASSWORD)
